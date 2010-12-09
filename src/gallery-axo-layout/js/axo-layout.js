d6734a794c78e0ab32125268b6c71bbc2d0fd3c4
Stjepan Rajko <stjepanr@axosoft.com>
2010-12-09 14:19:02 -0700

    added copyright notice to src file

--- src/gallery-axo-layout/js/axo-layout.js
+++ src/gallery-axo-layout/js/axo-layout.js
@@ -1,3 +1,9 @@
+/*
+Copyright (c) 2010, Axosoft, LLC. All rights reserved.
+Code licensed under the BSD License:
+http://developer.yahoo.com/yui/license.html
+*/
+
 var Event = Y.Event,
     Lang = Y.Lang,
 
